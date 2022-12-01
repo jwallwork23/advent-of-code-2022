2f5700e7bdc06870a83b4bc2269f7e298505d509
Joe Wallwork <joe.wallwork@outlook.com>
2022-12-01 07:21:47 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="advent-of-code-2022",
+    description="Advent of code solutions 2022",
+    author="Joseph Wallwork",
+    author_email="joe.wallwork@outlook.com",
+    url="https://github.com/jwallwork23/advent_of_code_2022",
+    version="0.1",
+    packages=["advent"],
+)
